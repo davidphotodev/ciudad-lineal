3134dd4077dbf78ed9662625800ef4e955225f62
SocratesCrespo <socratescrespo@gmail.com>
2023-07-17 11:43:25 +0200

    Structure and styles for dashboard

--- src/app/shared/layouts/dashboard/dashboard.component.ts
+++ src/app/shared/layouts/dashboard/dashboard.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.sass']
+  styleUrls: ['./dashboard.component.sass'],
+  standalone: true,
+  imports: [
+    RouterModule
+  ]
 })
 export class DashboardComponent {
 
