516b0a9ad07dde151c4378d18c70d532d3587e31
SocratesCrespo <socratescrespo@gmail.com>
2023-08-17 19:15:08 +0200

    Adding detail views

---
+++ src/app/modules/publishers/pages/detail/components/modals/finish-territory/finish-territory.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-finish-territory',
+  templateUrl: './finish-territory.component.html',
+  styleUrls: ['./finish-territory.component.sass']
+})
+export class FinishTerritoryComponent {
+  
+  @Output()
+  public childEvent = new EventEmitter<string>();
+
+  hideModal(){
+    this.childEvent.emit('d-none');
+  }
+
+}
--- src/app/modules/publishers/publishers.module.ts
+++ src/app/modules/publishers/publishers.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ListModule } from './pages/list/list.module';
 import { PublishersRoutingModule } from './publishers-routing.module';
 import { DetailComponent } from './pages/detail/detail.component';
+import { FinishTerritoryComponent } from './pages/detail/components/modals/finish-territory/finish-territory.component';
 
 
 
 @NgModule({
   declarations: [
-    DetailComponent
+    DetailComponent,
+    FinishTerritoryComponent
   ],
   imports: [
     CommonModule,
