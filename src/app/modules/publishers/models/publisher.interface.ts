ae0053a8657914af79b95dfdb6c85d2a8e9598fa
SocratesCrespo <socratescrespo@gmail.com>
2024-01-01 19:26:40 +0100

    Consulting publishers from database

--- src/app/modules/publishers/models/publisher.interface.ts
+++ src/app/modules/publishers/models/publisher.interface.ts
@@ -1,4 +1,5 @@
 export interface Publisher {
+    id?: string,
     firstname: string,
     lastname: string,
     publisherType: string,
--- src/app/modules/publishers/services/publishers.service.ts
+++ src/app/modules/publishers/services/publishers.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Firestore, addDoc, collection } from '@angular/fire/firestore';
+import { Firestore, addDoc, collection, collectionData } from '@angular/fire/firestore';
 import { Publisher } from '../models/publisher.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,10 @@ export class PublishersService {
   addPublisher( publisher: Publisher ){
     const publisherRef = collection(this.firestore, 'publishers');
     return addDoc( publisherRef, publisher );
+  }
+
+  getPublisher(): Observable<Publisher[]> {
+    const publisherRef = collection( this.firestore, 'publishers' );
+    return collectionData( publisherRef, { idField: 'id' } ) as Observable<Publisher[]>;
   }
 }
