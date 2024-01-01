ba502685e79e9d16cd7c7ca1c98420402f157de7
SocratesCrespo <socratescrespo@gmail.com>
2024-01-01 19:27:33 +0100

    Showing publisher list from database

--- src/app/modules/publishers/pages/list/list.component.ts
+++ src/app/modules/publishers/pages/list/list.component.ts
@@ -1,34 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PublishersService } from '../../services/publishers.service';
+import { Publisher } from '../../models/publisher.interface';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.sass']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
 
   public displayCount = 9;
 
-  public publishers: any[] = [
-    {
-      'id': 1,
-      'name': 'David Adan',
-      'detail': 'Precursor',
-      'territories': '35, 46'
-    },
-    {
-      'id': 2,
-      'name': 'Sócrates Crespo',
-      'detail': 'Publicador',
-      'territories': '169'
-    },
-    {
-      'id': 3,
-      'name': 'Joan Mendez',
-      'detail': 'Publicador',
-      'territories': '78'
-    }
-  ];
+  public publishers: Publisher[] = [];
+
+  constructor( private publishersService: PublishersService ){}
+
+  ngOnInit(): void {
+    this.publishersService.getPublisher()
+      .subscribe(
+        publishers => {
+          this.publishers = publishers;
+        }
+      )
+  }
 
   viewMore(){
     this.displayCount = this.displayCount + 9;
