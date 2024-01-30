2de5e1dcb0f36b6103a01b29b21f421d9708f869
SocratesCrespo <socratescrespo@gmail.com>
2024-01-30 13:13:48 +0100

    More dynamic content for publisher detail page

--- src/app/modules/territories/pages/detail/detail.component.ts
+++ src/app/modules/territories/pages/detail/detail.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TerritoriesService } from '../../services/territories.service';
+import { ActivatedRoute } from '@angular/router';
+import { Territory } from '../../models/territories.interface';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.sass']
 })
-export class DetailComponent {
-
+export class DetailComponent implements OnInit {
+  
   public finishModalClass: string = 'd-none';
   public whatsappNumber: string = '604216037';
+  public territory!: Territory;
+
+  constructor( private territoriesService: TerritoriesService,
+               private activatedRoute: ActivatedRoute ){}
+  
+  
+  async ngOnInit() {
+    this.activatedRoute.params
+      .subscribe(
+        async ({ id }) => {
+          const territoryData = await this.territoriesService.getTerritoryById( id );
+          this.territory = territoryData;
+          console.log(this.territory.history)
+        }
+      );
+  }
+
 
   hideModal( value: string ){
     this.finishModalClass = value;
