b2e9b81b689fbe9428af407c9bf0d454621f7a5f
SocratesCrespo <socratescrespo@gmail.com>
2024-02-20 13:27:20 +0100

    Creating filters button to territories list

--- src/app/modules/territories/pages/list/list.component.ts
+++ src/app/modules/territories/pages/list/list.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TerritoriesService } from '../../services/territories.service';
 import { Territory } from '../../models/territories.interface';
+import { Subject, takeUntil } from 'rxjs';
+import * as moment from 'moment';
+import { DatesService } from 'src/app/core/services/dates.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.sass']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, OnDestroy {
 
   public displayCount: number = 5;
   public territories: Territory[] = [
@@ -22,20 +25,57 @@ export class ListComponent implements OnInit {
       date_assigned: '',
       last_date: 0
     }
-  ]
+  ];
+  public list!: Territory[];
+  public listState: string = 'todos';
+  destroyObs$: Subject<void> = new Subject();
 
-  constructor( private territoriesService: TerritoriesService ){}
+  constructor( private territoriesService: TerritoriesService,
+               private datesService: DatesService ){}
 
   ngOnInit(): void {
     this.territoriesService.getTerritories()
+      .pipe( takeUntil( this.destroyObs$ ) )
       .subscribe(
         territories => {
           this.territories = territories;
+          this.list = territories;
         })
+  }
+
+  ngOnDestroy(): void {
+    this.destroyObs$.next();
+    this.destroyObs$.complete();
   }
 
   viewMore(){
     this.displayCount = this.displayCount + 9;
+  }
+
+  filterList( state: string ){
+    if( state == 'todos' ){
+      this.list = this.territories;
+      this.listState = 'todos';
+      return;
+    }
+
+    this.list = this.territories
+      .filter( territory => territory.state == state )
+      .sort( (a,b) => a.last_date - b.last_date );
+
+    this.listState = state;
+      
+  }
+
+  endDate( date: number ){
+    const finish = Number(date);
+    const end_date = moment( finish, 'YYYYMMDD' ).format('DD-MM-YYYY');
+
+    return end_date;
+  }
+
+  toExpire( date: number ): number{
+    return this.datesService.dateToExpire( date );
   }
 
 }
