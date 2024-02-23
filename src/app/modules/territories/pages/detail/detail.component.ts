292a8fad63956c7b67801d374e2cb1898e29f2c8
SocratesCrespo <socratescrespo@gmail.com>
2024-02-23 16:36:09 +0100

    Send whatsapp from territory detail page

--- src/app/modules/territories/pages/detail/detail.component.ts
+++ src/app/modules/territories/pages/detail/detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TerritoriesService } from '../../services/territories.service';
 import { ActivatedRoute } from '@angular/router';
 import { Territory } from '../../models/territories.interface';
+import { PublishersService } from 'src/app/modules/publishers/services/publishers.service';
 
 @Component({
   selector: 'app-detail',
@@ -17,7 +18,8 @@ export class DetailComponent implements OnInit {
   public lastDate!: string;
 
   constructor( private territoriesService: TerritoriesService,
-               private activatedRoute: ActivatedRoute ){}
+               private activatedRoute: ActivatedRoute,
+               private publishersService: PublishersService ){}
   
   
   async ngOnInit() {
@@ -26,15 +28,10 @@ export class DetailComponent implements OnInit {
         async ({ id }) => {
           const territoryData = await this.territoriesService.getTerritoryById( id );
           if( territoryData ){
-            this.territory = territoryData;
-            this.territory.id = id;
+            this.territory = { id, ...territoryData };
           }
         }
       );
-  }
-
-  assignTerritory( id: string ){
-
   }
 
   hideModal( value: string ){
@@ -43,6 +40,16 @@ export class DetailComponent implements OnInit {
 
   hideDeleteModal( value: string ){
     this.deleteModalClass = value;
+  }
+
+  async sendWhatsapp(){
+    if( this.territory.publisher_id === '' || this.territory.publisher_id === undefined ) return;
+    
+    const publisher = await this.publishersService.getPublisherById( this.territory.publisher_id );
+    const message = encodeURIComponent( `¡Hola ${ publisher.firstname }! \n\nSe te ha asignado el territorio *${ this.territory.number }* \n\nPuedes ver el mapa aquí: ${ this.territory.map }` );
+
+    const url = `https://wa.me/34${ publisher.whatsapp }?text=${ message }`;
+    window.open( url, '_blank' );
   }
 
 }
