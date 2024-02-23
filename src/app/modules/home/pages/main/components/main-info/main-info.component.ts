3d69f845b126c94c5adec14b70abbe8f35ad1916
SocratesCrespo <socratescrespo@gmail.com>
2024-02-23 14:06:56 +0100

    Destroying subscription in main page

--- src/app/modules/home/pages/main/components/main-info/main-info.component.ts
+++ src/app/modules/home/pages/main/components/main-info/main-info.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 import { DatesService } from 'src/app/core/services/dates.service';
 import { PublishersService } from 'src/app/modules/publishers/services/publishers.service';
 import { TerritoriesService } from 'src/app/modules/territories/services/territories.service';
@@ -8,12 +9,13 @@ import { TerritoriesService } from 'src/app/modules/territories/services/territo
   templateUrl: './main-info.component.html',
   styleUrls: ['./main-info.component.sass']
 })
-export class MainInfoComponent implements OnInit {
+export class MainInfoComponent implements OnInit, OnDestroy {
 
   public publishers: number = 0;
   public assigned: number = 0;
   public available: number = 0;
   public finished: number = 0;
+  destroyObs$: Subject<void> = new Subject();
 
   constructor( private publishersService: PublishersService,
                private territoriesService:TerritoriesService,
@@ -21,18 +23,25 @@ export class MainInfoComponent implements OnInit {
 
   ngOnInit() {
     this.publishersService.getPublishers()
-        .subscribe(
-          publishers => this.publishers = publishers.length
-        );
+      .pipe( takeUntil( this.destroyObs$ ) )    
+      .subscribe(
+        publishers => this.publishers = publishers.length
+      );
 
     this.territoriesService.getTerritories()
-        .subscribe(
-          territories => {
-            this.assigned = territories.filter( territory => territory.state === 'Assigned' ).length;
-            this.available = territories.filter( territory => territory.state === 'Not assigned' && this.lastDate(Number(territory.last_date)) >= 3 ).length;
-            this.finished = territories.filter( territory => territory.state === 'Not assigned' && this.lastDate(Number(territory.last_date)) < 3 ).length;
-          }
-        );
+      .pipe( takeUntil( this.destroyObs$ ) )    
+      .subscribe(
+        territories => {
+          this.assigned = territories.filter( territory => territory.state === 'Assigned' ).length;
+          this.available = territories.filter( territory => territory.state === 'Not assigned' && this.lastDate(Number(territory.last_date)) >= 3 ).length;
+          this.finished = territories.filter( territory => territory.state === 'Not assigned' && this.lastDate(Number(territory.last_date)) < 3 ).length;
+        }
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.destroyObs$.next();
+    this.destroyObs$.complete();
   }
 
   lastDate( date: number ){
